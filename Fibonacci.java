b65962f3fe404d5d07bf0702ad60af0e7a3d78c9
sivapriyasri <sivapriyasri066@gmail.com>
2026-05-17 20:28:33 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        int a = 0, b = 1;
+
+        for(int i = 1; i <= 10; i++) {
+
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
